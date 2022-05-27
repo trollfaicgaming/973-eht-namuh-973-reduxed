a29c70f3d67d298f232f8f98eee0df526b71656a
trollfaicgaming <94854693+trollfaicgaming@users.noreply.github.com>
2022-05-27 11:03:02 -0400

    Add files via upload

---
+++ 973/in.php
@@ -0,0 +1,8 @@
+// (☞ﾟ∀ﾟ)☞ static31
+if( window._cgen ) {
+_cgen.set_cookie('_jsuid', '3533746988');
+_cgen.advanced();
+_cgen.ping_start();
+_cgen.set_cookie('_first_pageview', 1, 600 );
+_cgen.set_cookie('_referrer_og', 'http://visualsitemapper.com/', 30*86400 );
+}
